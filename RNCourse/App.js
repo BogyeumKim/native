1c80c4facbae330fdf24f938d856c749922e6a9c
bogyeumkim <namastae112@gmail.com>
2024-01-10 21:32:26 +0900

    15. 컴포넌트 테스트

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View ,Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>new World</Text>
+      </View>
+      <Text>Hello</Text>
+      <Text>World</Text>
+      <Button title='touch' />
     </View>
   );
 }
