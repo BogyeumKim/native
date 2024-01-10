f116d972af95f68efeff2e1afc9a3471d897405e
bogyeumkim <namastae112@gmail.com>
2024-01-10 22:54:56 +0900

    20. 플렉스박스

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View ,Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='입력하는곳임'/>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} placeholder='입력하는곳임'/>
         <Button title='Goal'></Button>
       </View>
       <View>
@@ -20,5 +20,18 @@ export default function App() {
 const styles = StyleSheet.create({
  appContainer:{
   padding:50
+ },
+
+ inputContainer: {
+  flexDirection:'row',
+  justifyContent: 'space-between'
+ },
+
+ TextInput:{
+  borderWidth:1,
+  borderColor: '#cccccc',
+  width:'80%',
+  marginRight :8,
+  padding: 8,
  }
 });
