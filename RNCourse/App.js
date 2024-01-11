291b2cc6d6c72d0006b6a1fe025c79c19ae3af5d
bogyeumkim <namastae112@gmail.com>
2024-01-11 22:30:34 +0900

    22. 플렉스 레이아웃 개선

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,37 +1,52 @@
-
 import { StyleSheet, Text, View ,Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.appContainer}>
+
       <View style={styles.inputContainer}>
         <TextInput style={styles.TextInput} placeholder='입력하는곳임'/>
         <Button title='Goal'></Button>
       </View>
-      <View>
+
+      <View style={styles.goalsContainer}>
         <Text>
           List of goals...
         </Text>
       </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
  appContainer:{
-  padding:50
+  flex : 1,
+  padding:50,
+  paddingHorizontal:16
  },
 
  inputContainer: {
+  flex : 1,
   flexDirection:'row',
-  justifyContent: 'space-between'
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom : 24,
+  borderBottomWidth : 1,
+  borderColor : '#cccccc'
  },
 
  TextInput:{
   borderWidth:1,
   borderColor: '#cccccc',
-  width:'80%',
+  width:'70%',
   marginRight :8,
   padding: 8,
+ },
+
+ goalsContainer: {
+  flex:5
  }
+
+ 
 });
