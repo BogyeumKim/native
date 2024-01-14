458d0bc714c4fdd85280dab87ae6e4de9227c2ae
bogyeumkim <namastae112@gmail.com>
2024-01-14 17:04:02 +0900

    27. FlatList
    필요한 부분만 랜더링시킴

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View ,Button, TextInput,ScrollView } from 'react-native';
+import { StyleSheet, Text, View ,Button, TextInput,ScrollView , FlatList } from 'react-native';
 
 export default function App() {
 
@@ -11,7 +11,7 @@ export default function App() {
   };
 
   function addHandler() {
-    setCourse(currentCourse => [...currentCourse,enteredText])
+    setCourse(currentCourse => [...currentCourse,{text : enteredText, id : Math.random().toString()}])
   };
 
 
@@ -24,13 +24,21 @@ export default function App() {
       </View>
       
       <View style={styles.goalsContainer}>
-        <ScrollView alwaysBounceVertical={false}>
-          {course.map((item,index) =>
-          <View style={styles.goalItem} key={index}>
-            <Text style={styles.goalText}>{item}</Text>
-          </View>
-          )}
-        </ScrollView>
+        <FlatList data={course} renderItem={itemData => {
+          return (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText}>{itemData.item.text}</Text>
+            </View>
+          );
+        }} 
+        keyExtractor={(item, index)=>{
+          return item.id;
+        }}
+        alwaysBounceVertical={false}>
+         
+        
+         
+        </FlatList>
       </View>
     </View>
   );
