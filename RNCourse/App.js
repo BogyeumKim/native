8945f8e8d1a03aa459f9bd37aa491da2ac006cf6
bogyeumkim <namastae112@gmail.com>
2024-01-14 17:58:34 +0900

    30. 컴포넌트화 2

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View ,Button, TextInput , FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
 
-  const [enteredText, setEnteredText] = useState('');
+
   const [course, setCourse] = useState([]);
 
-  function inputHandler(enteredText) {
-    setEnteredText(enteredText);
-  };
 
-  function addHandler() {
+  function addHandler(enteredText) {
     setCourse(currentCourse => [...currentCourse,{text : enteredText, id : Math.random().toString()}])
   };
 
@@ -19,14 +17,11 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
 
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.TextInput} placeholder='입력하는곳임' onChangeText={inputHandler} />
-        <Button title='Goal' onPress={addHandler} />
-      </View>
+      <GoalInput onAddGoal={addHandler} />
       
       <View style={styles.goalsContainer}>
         <FlatList data={course} renderItem={itemData => {
-          return <GoalItem text={itemData.item.text}/>;
+          return <GoalItem item={itemData.item}/>;
         }} 
         keyExtractor={(item, index)=>{
           return item.id;
@@ -48,23 +43,6 @@ const styles = StyleSheet.create({
   paddingHorizontal:16
  },
 
- inputContainer: {
-  flex : 1,
-  flexDirection:'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  marginBottom : 24,
-  borderBottomWidth : 1,
-  borderColor : '#cccccc'
- },
-
- TextInput:{
-  borderWidth:1,
-  borderColor: '#cccccc',
-  width:'70%',
-  marginRight :8,
-  padding: 8,
- },
 
  goalsContainer: {
   flex:5
---
+++ RNCourse/components/GoalInput.js
@@ -0,0 +1,49 @@
+import {View, TextInput , Button, StyleSheet} from 'react-native'
+import { useState } from 'react';
+
+export default function GoalInput(props){
+    const [enteredText, setEnteredText] = useState('');
+
+    function inputHandler(textVal) {
+        setEnteredText(textVal);
+      };
+
+      function addHandler(){
+        props.onAddGoal(enteredText);
+        setEnteredText('');
+      }
+
+    return (
+        <View style={styles.inputContainer}>
+        <TextInput 
+        style={styles.TextInput} 
+        placeholder='입력하는곳임' 
+        onChangeText={inputHandler} 
+        value={enteredText}
+        />
+        
+        <Button title='Goal' onPress={addHandler} />
+      </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+    inputContainer: {
+        flex : 1,
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom : 24,
+        borderBottomWidth : 1,
+        borderColor : '#cccccc'
+       },
+      
+       TextInput:{
+        borderWidth:1,
+        borderColor: '#cccccc',
+        width:'70%',
+        marginRight :8,
+        padding: 8,
+       },
+})
