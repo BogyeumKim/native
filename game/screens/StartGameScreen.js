bee748cfd5b0589cf0b98ce150fc3f8e8e616dbb
bogyeumkim <namastae112@gmail.com>
2024-01-16 22:15:01 +0900

    50. 안드로이드 및 아이폰 보더 그림자

--- game/screens/StartGameScreen.js
+++ game/screens/StartGameScreen.js
@@ -1,10 +1,10 @@
-import {TextInput,View} from 'react-native'
+import {StyleSheet, TextInput,View} from 'react-native'
 import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
 
     return ( 
-    <View>
-        <TextInput />
+    <View style={styles.inputContainer}>
+        <TextInput/>
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -12,5 +12,23 @@ function StartGameScreen() {
     
 
 }
+
+const styles = StyleSheet.create({
+    inputContainer : {
+        // flex : 1 ,
+        padding : 16 ,
+        marginTop : 100,
+        marginHorizontal : 16,
+        backgroundColor: '#72063c',
+        borderRadius : 8,
+        elevation : 4, // 안드로이드 전용 , border끝에 그림자효과
+
+        /** 아이폰 전용 */ 
+        shadowColor : 'black',
+        shadowOffset : {width : 0, height : 2}, // 아래쪽으로 2
+        shadowRadius : 6,
+        shadowOpacity : 0.25,
+    }
+});
 
 export default StartGameScreen;
