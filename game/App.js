0d85b13197675135f9f9e3ebdf706d8ab38003e0
bogyeumkim <namastae112@gmail.com>
2024-01-17 23:37:01 +0900

    57. 배경이미지 추가

--- game/App.js
+++ game/App.js
@@ -1,12 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
-import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground source={require('./assets/images/background.png')} resizeMode="cover" style={styles.rootScreen}
+      imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,6 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     // backgroundColor: "#ddb52f",
-    flex:1,
+    flex: 1,
   },
+  backgroundImage : {
+    opacity : 0.35,
+  }
 });
