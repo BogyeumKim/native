030ae9a9ea44417f1cabd7503843d0aeac2caa50
bogyeumkim <namastae112@gmail.com>
2024-01-19 23:46:22 +0900

    65. 무작위수 생성 및 컴포넌트 분리

--- game/components/ui/Title.js
+++ game/components/ui/Title.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text } from "react-native";
-import Colors from "../constants/colors";
-export default function Title({children}){
+
+function Title({children}){
     return <Text style={styles.title}>{children}</Text>
 }
 
@@ -8,11 +8,12 @@ const styles = StyleSheet.create({
     title : {
         fontSize : 24,
         fontWeight : 'bold',
-        color : Colors.accent500,
+        color : 'white',
         textAlign : 'center',
         borderWidth : 2,
-        borderColor : Colors.accent500,
+        borderColor : 'white',
         padding:12,
       },
       
 })
+export default Title;
--- game/screens/GameScreen.js
+++ game/screens/GameScreen.js
@@ -1,10 +1,27 @@
 import { Text, View, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import  { useState } from 'react'
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen() {
+
+function generateRandomBetween(min , max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if( rndNum === exclude ){
+    return generateRandomBetween(min, max, exclude);
+  }else{
+    return rndNum;
+  }
+}
+
+function GameScreen({userNumber}) {
+
+  const initalGuess = generateRandomBetween(1,100, userNumber)
+  const [currentGuess, setCurrentGuess] = useState(initalGuess);
   return (
     <View style={styles.screen}>
       <Title>Opponents'Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       {/* GUESS */}
       <View>
         <Text>Higher or lower?</Text>
