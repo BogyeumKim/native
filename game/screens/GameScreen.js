7cfed88534d97c8c3a7fb76c94b8fbbe329cffb0
bogyeumkim <namastae112@gmail.com>
2024-01-21 21:12:47 +0900

    78. 시도 회수 적용하기

--- game/screens/GameScreen.js
+++ game/screens/GameScreen.js
@@ -29,7 +29,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
   useEffect(() => {
     if (currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
 
@@ -88,7 +88,7 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      <View>
+      <View style={styles.listContainer}>
         {/* {guessRounds.map(guessRound =><Text key={guessRound}>{guessRound}</Text>)} */}
         <FlatList data={guessRounds} renderItem={(itemData) => <GuessLogItem roundNumber={guessRoundsListLength - itemData.index} guess={itemData.item}  />}
         keyExtractor={(item) => item}
@@ -114,4 +114,8 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flex: 1,
   },
+  listContainer : {
+    flex:1,
+    padding:16,
+  }
 });
