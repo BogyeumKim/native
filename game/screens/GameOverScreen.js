8c7b8015930eb35edd0fb057f3f3532d16c1de4f
bogyeumkim <namastae112@gmail.com>
2024-01-21 22:44:09 +0900

    88. useWindowDimensions 를 이용한 동적 UI 2

--- game/screens/GameOverScreen.js
+++ game/screens/GameOverScreen.js
@@ -1,30 +1,64 @@
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+} from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
 
-export default function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
+export default function GameOverScreen({
+  roundsNumber,
+  userNumber,
+  onStartNewGame,
+}) {
+  const { width, height } = useWindowDimensions();
+  let imageSize = 300;
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 500) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  };
+
   return (
-    <View style={styles.rootContainer}>
-      <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../assets/images/success.png")}
-          style={styles.image}
-        />
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>GAME OVER!</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image
+            source={require("../assets/images/success.png")}
+            style={styles.image}
+          />
+        </View>
+        <Text style={styles.summaryText}>
+          Your phone needex <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
+          rounds to guess the number{" "}
+          <Text style={styles.highlight}>{userNumber}</Text>
+        </Text>
+        <PrimaryButton onpress={onStartNewGame}>Start New Game</PrimaryButton>
       </View>
-      <Text style={styles.summaryText}>
-        Your phone needex <Text style={styles.highlight}>{roundsNumber}</Text> rounds to
-        guess the number <Text style={styles.highlight}>{userNumber}</Text>
-      </Text>
-      <PrimaryButton onpress={onStartNewGame}>Start New Game</PrimaryButton>
-    </View>
+    </ScrollView>
   );
 }
 
-const deviceWidht = Dimensions.get('window').width;
+// const deviceWidht = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
@@ -33,9 +67,9 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     borderRadius: 150,
-    width: Dimensions<380 ? 150 :300,
-    height: Dimensions < 380 ? 150 : 300,
-    borderWidth: Dimensions < 380 ? 75 : 150,
+    // width: deviceWidht < 380 ? 150 :300,
+    // height: deviceWidht < 380 ? 150 : 300,
+    // borderWidth: deviceWidht < 380 ? 75 : 150,
     borderColor: Colors.primary800,
     overflow: "hidden",
     margin: 36,
@@ -46,9 +80,9 @@ const styles = StyleSheet.create({
   },
   summaryText: {
     fontFamily: "open-sans",
-    fontSize:24,
-    textAlign:'center',
-    marginBottom:24,
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
   },
   highlight: {
     fontFamily: "open-sans-bold",
