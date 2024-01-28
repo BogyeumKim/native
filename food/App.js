ea9486203820b7d6055510cbac9133fe86bdc3bb
bogyeumkim <namastae112@gmail.com>
2024-01-28 14:58:20 +0900

    95. 네비게이션 추가

--- food/App.js
+++ food/App.js
@@ -1,12 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import {createNativeStackNavigator } from '@react-navigation/native-stack'
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
