d8c9bfed5975ad361139c1718903cec960f6634f
bogyeumkim <namastae112@gmail.com>
2024-01-28 18:46:24 +0900

    103. 네비게이션 옵션 동적 설정

--- food/screens/MealsOverviewScreen.js
+++ food/screens/MealsOverviewScreen.js
@@ -1,9 +1,10 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS , CATEGORIES } from "../data/dummy-data";
 import { useRoute } from "@react-navigation/native";
 import MealItem from "../components/MealItem";
+import { useEffect, useLayoutEffect } from "react";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route , navigation }) {
   const rout = useRoute();
   const catId = rout.params.categoryId;
   // const catId = route.params.categoryId;
@@ -11,6 +12,17 @@ function MealsOverviewScreen({ route }) {
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+
+    navigation.setOptions({
+        title : categoryTitle,
+      });
+
+  },[catId,navigation])
+  
 
   function renderMealItem(itemData) {
 
