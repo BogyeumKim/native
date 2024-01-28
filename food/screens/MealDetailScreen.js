a5dbcf2f030b2c4f3d6b5a102af40390cc97d5de
bogyeumkim <namastae112@gmail.com>
2024-01-28 20:46:09 +0900

    108. 헤더에 아이콘추가

--- food/screens/MealDetailScreen.js
+++ food/screens/MealDetailScreen.js
@@ -12,6 +12,7 @@ import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import { useLayoutEffect } from "react";
+import IconButton from "../components/IconButton";
 function MealDetailScreen({ route , navigation }) {
   const mealId = route.params.mealId;
 
@@ -25,7 +26,8 @@ function MealDetailScreen({ route , navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
         headerRight: () =>{
-            return <Button title="Tap me" onPress={headerButtonPressHandler} />
+            // return <Button title="Tap me" onPress={headerButtonPressHandler} />
+            return <IconButton onPress={headerButtonPressHandler} icon='star' color='white' />
         }
     })
   },[navigation,headerButtonPressHandler])
