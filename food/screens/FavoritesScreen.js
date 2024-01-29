c76311494e07051edeb7f8128c22c2c7081ab5d0
bogyeumkim <namastae112@gmail.com>
2024-01-29 23:02:30 +0900

    119. ContextApi로 즐겨찾기 관리하기

--- food/screens/FavoritesScreen.js
+++ food/screens/FavoritesScreen.js
@@ -1,7 +1,38 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-    return (<Text>즐겨찾기 스크린</Text>  );
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoirteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  if (favoirteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>즐겨찾기가 없어요.</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoirteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
