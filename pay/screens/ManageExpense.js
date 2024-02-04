3fda9c1f9c73966cafde351249ef823a23a7bbc2
bogyeumkim <namastae112@gmail.com>
2024-02-04 22:21:01 +0900

    141. 커스텀버튼 추가

--- pay/screens/ManageExpense.js
+++ pay/screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -15,9 +16,18 @@ function ManageExpense({ route, navigation }) {
 
   function deleteExpenseHandler() {}
 
+  function cancelhandler() {}
+
+  function confirmHandler() {}
+
   return (
     <View style={styles.container}>
-        
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelhandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>{isEditing ? "Update" : "Add"}</Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -34,16 +44,25 @@ function ManageExpense({ route, navigation }) {
 
 export default ManageExpense;
 const styles = StyleSheet.create({
-    container : {
-        flex:1,
-        padding:24,
-        backgroundColor : GlobalStyles.colors.primary800
-    },
-    deleteContainer : {
-            marginTop : 16,
-            paddingTop : 8,
-            borderTopWidth : 2,
-            borderTopColor : GlobalStyles.colors.primary200,
-            alignItems : 'center',
-    }
-})
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons : {
+    flexDirection : 'row',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button:{
+    minWidth : 120,
+    marginHorizontal:8
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
