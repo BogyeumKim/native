61e707930007bae663ba9b4fb8fd683340dbec15
bogyeumkim <namastae112@gmail.com>
2024-02-04 17:09:38 +0900

    134. 지출 리스트 아이템

--- pay/components/ExpensesOutput.js
+++ pay/components/ExpensesOutput.js
@@ -34,6 +34,35 @@ const DUMMY_EXPENSES = [
         description : '반지',
         amount : 29.99,
         date : new Date('2024-01-11')
+    }, {
+        id: 'e6',
+        description : '신발',
+        amount : 59.99,
+        date : new Date('2024-02-01')
+    },
+    {
+        id: 'e7',
+        description : '티셔츠',
+        amount : 109.99,
+        date : new Date('2024-01-05')
+    },
+    {
+        id: 'e8',
+        description : '청바지',
+        amount : 89.99,
+        date : new Date('2024-01-20')
+    },
+    {
+        id: 'e9',
+        description : '자켓',
+        amount : 209.99,
+        date : new Date('2024-02-09')
+    },
+    {
+        id: 'e10',
+        description : '반지',
+        amount : 29.99,
+        date : new Date('2024-01-11')
     },
 ];
 
@@ -51,7 +80,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        padding : 24,
+        paddingHorizontal:24,
+        paddingTop:24,
+        paddingBottom:0,
         backgroundColor :GlobalStyles.colors.primary700
     },
 })
