01538ee5540c541b4760d90908c0ac1bcc9eb600
bogyeumkim <namastae112@gmail.com>
2024-02-07 22:50:48 +0900

    153. useSate를 이용해 사용자 입력 처리하기

--- pay/components/ManageExpense/ExpenseForm.js
+++ pay/components/ManageExpense/ExpenseForm.js
@@ -1,8 +1,22 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import Input from "./Input";
+import { useState } from "react";
 
 function ExpenseForm() {
-  function amountChangeHandler() {}
+  const [inputValues, setInputValues] = useState({
+    amount : '',
+    date:'',
+    description:''
+  });
+
+  function inputChangedHandler(inputIdentifier,enteredValue) {
+    setInputValues((curInputValues) =>{
+      return {
+        ...curInputValues,
+        [inputIdentifier] : enteredValue
+      };
+    })
+  }
 
   return (
     <View style={styles.form}>
@@ -13,7 +27,8 @@ function ExpenseForm() {
           label="Amount"
           textInputConfig={{
             keyboardType: "decimal-pad",
-            onChangeText: amountChangeHandler,
+            onChangeText: inputChangedHandler.bind(this, 'amount'),
+            value : inputValues.amount,
           }}
         />
         <Input
@@ -22,7 +37,8 @@ function ExpenseForm() {
           textInputConfig={{
             placeholder: "YYYY-MM-DD",
             maxLength: 10,
-            onChangeText: () => {},
+            onChangeText: inputChangedHandler.bind(this, 'date'),
+            value : inputValues.date,
           }}
         />
       </View>
@@ -33,6 +49,8 @@ function ExpenseForm() {
           multiline: true,
           autoCapitalize: "none", // 대소문자 방지
           // autoCorrect : false, // default
+          onChangeText : inputChangedHandler.bind(this, 'description'),
+          value : inputValues.description,
         }}
       />
     </View>
