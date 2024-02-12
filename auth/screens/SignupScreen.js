b1bee2cada0d236516d0f2fb602092a9587195ce
bogyeumkim <namastae112@gmail.com>
2024-02-12 22:31:47 +0900

    180. 에러 핸들링

--- auth/screens/SignupScreen.js
+++ auth/screens/SignupScreen.js
@@ -2,14 +2,19 @@ import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import { createUser } from '../util/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay'
+import { Alert } from 'react-native';
 
 function SignupScreen() {
   const [isAuthenticationg, setIsAuthenticating] = useState(false);
 
 
   async function signuphandler({email, password}){
-    setIsAuthenticating(true);
-    await createUser(email, password);
+    try{
+      setIsAuthenticating(true);
+      await createUser(email, password);  
+    }catch(e){
+        Alert.alert('인증 실패!', '사용자 생성에 실패하였습니다.');
+    }
     setIsAuthenticating(false);
   }
 
