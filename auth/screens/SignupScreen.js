0eab40a71edf9e3fa581c0024c2e7ddbef732882
bogyeumkim <namastae112@gmail.com>
2024-02-12 22:21:25 +0900

    178. 새로운 사용자 생성하기

--- auth/screens/SignupScreen.js
+++ auth/screens/SignupScreen.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay'
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticationg, setIsAuthenticating] = useState(false);
+
+
+  async function signuphandler({email, password}){
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if(isAuthenticationg) {
+    return <LoadingOverlay  message="생성중 ..." />
+  }
+
+  return <AuthContent onAuthenticate={signuphandler}/>;
 }
 
 export default SignupScreen;
