8fd1fbe3c26ded8e05e1db5b7c74ddc1037b886c
bogyeumkim <namastae112@gmail.com>
2024-02-17 23:55:30 +0900

    185. 파이어베이스 보호된 자료에 접근하기

--- auth/screens/WelcomeScreen.js
+++ auth/screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchMessage] = useState("");
+  const authReducer = useSelector(state => state.authReducer);
+  const token = authReducer.token;
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-course-b79fe-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=" + token
+      )
+      .then((res) => {
+        setFetchMessage(res.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +31,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
