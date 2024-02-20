c9439b7a7e5dd7b1ffabac3e341eded677dd567c
bogyeumkim <namastae112@gmail.com>
2024-02-20 22:44:07 +0900

    194. 글로벌 header style

--- nativeFunction/App.js
+++ nativeFunction/App.js
@@ -5,6 +5,7 @@ import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import { Text } from "react-native";
 import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,11 +14,18 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle:{ backgroundColor:Colors.primary500},
+          headerTintColor : Colors.gray700,
+          contentStyle : {backgroundColor : Colors.gray700}
+        }}>
           <Stack.Screen name="AllPlaces" component={AllPlaces} options={ ({navigation}) => ({
+            title : 'Your Favorite Places',
             headerRight : ({tintColor}) => <IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate('AddPlace')}/>
           }) }/>
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen name="AddPlace" component={AddPlace} options={{
+            title:'Add a new Place'
+          }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- nativeFunction/components/Places/PlacesList.js
+++ nativeFunction/components/Places/PlacesList.js
@@ -1,12 +1,14 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
 
 function PlacesList({ places }) {
   if (!places || places.length === 0) {
-
     return (
-      <View styles={styles.fallbackContainer}>
-        <Text style={styles.fallbackText}> No places added yet - start adding some!</Text>
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          No places added yet - start adding some!
+        </Text>
       </View>
     );
   }
@@ -24,13 +26,13 @@ function PlacesList({ places }) {
 export default PlacesList;
 
 const styles = StyleSheet.create({
-  fallbackContainer : {
-    flex : 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  } , 
-  fallbackText : {
-    fontSize : 16
-  }
-  
-})
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+    color:Colors.primary200
+  },
+});
