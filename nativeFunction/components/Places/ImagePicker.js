a38d61963971268d898229fb3f03f3167623a3b6
bogyeumkim <namastae112@gmail.com>
2024-02-21 23:56:52 +0900

    199. 이미지 미리보기 표시하기

--- nativeFunction/components/Places/ImagePicker.js
+++ nativeFunction/components/Places/ImagePicker.js
@@ -1,10 +1,14 @@
-import { View, Button, Alert } from "react-native";
+import { View, Button, Alert, Image, Text, StyleSheet } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
 function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -26,24 +30,48 @@ function ImagePicker() {
   async function takeImageHandler() {
     const hasPermission = await verifyPermissions();
 
-    if(!hasPermission) {
-        return;
+    if (!hasPermission) {
+      return;
     }
-    
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+
+    setPickedImage(image.assets[0].uri);
+  }
+
+  let imagePreview = <Text>이미지가 없음</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
       <Button title="Take Image" onPress={takeImageHandler} />
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview : {
+    width:'100%',
+    height : 200,
+    marginVertical : 8,
+    justifyContent : 'center',
+    alignItems : 'center',
+    backgroundColor : Colors.primary100,
+    borderRadius:4
+  },
+
+  image : {
+    width:'100%',
+    height:'100%'
+  }
+})
