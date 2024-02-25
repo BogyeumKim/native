e8372a89dc4a51d4e40bf3d4c8bb1c14d625ec91
bogyeumkim <namastae112@gmail.com>
2024-02-25 20:49:16 +0900

    205. 마커 추가하기

--- nativeFunction/screens/Map.js
+++ nativeFunction/screens/Map.js
@@ -1,20 +1,47 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView, {Marker} from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 function Map() {
-    const region = {
-            latitude : 37.55719830000000,
-            longitude : 127.19840589999997,
-            latitudeDelta: 0.0922,
-            longitudeDelta : 0.0429
-    };
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+  const [selectedLocation, setSelectedLocation] = useState();
+
+  const region = {
+    latitude: 37.5571983,
+    longitude: 127.19840589999997,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0429,
+  };
+
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="선택한 장소"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
 
 const styles = StyleSheet.create({
-    map : {
-        flex : 1
-    }
-})
+  map: {
+    flex: 1,
+  },
+});
