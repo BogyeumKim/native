a6dafe0935f2b825831578a789d06f8a5618db44
bogyeumkim <namastae112@gmail.com>
2024-02-26 21:41:16 +0900

    215. PLACE INSERT 추가

--- nativeFunction/util/database.js
+++ nativeFunction/util/database.js
@@ -3,11 +3,10 @@ import * as SQLite from "expo-sqlite";
 const database = SQLite.openDatabase("places.db");
 
 export function init() {
-
-    const promise = new Promise((resolve ,reject) => {
-        database.transaction((tx) => {
-            tx.executeSql(
-              `CREATE TABLE IF NOT EXISTS places (
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS places (
                     id INTEGER PRIMARY KEY NOT NULL,
                     title TEXT NOT NULL,
                     imageUri TEXT NOT NULL,
@@ -15,17 +14,43 @@ export function init() {
                     lat REAL NOT NULL,
                     lng REAL NOT NULL
                 )`,
-              [],
-              //   아래 위쪽은 성공시 콜백 아래는 에러 _,는 인자임 안쓰니까 블랭크
-              () => {
-                resolve()
-              },
-              (_, error) => {
-                reject(error);
-              }
-            );
-          });
+        [],
+        //   아래 위쪽은 성공시 콜백 아래는 에러 _,는 인자임 안쓰니까 블랭크
+        () => {
+          resolve();
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
     });
+  });
 
-    return promise;
+  return promise;
+}
+
+export function insertPlace(place) {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?,?,?,?,?)`,
+        [
+          place.title,
+          place.imageUri,
+          place.address,
+          place.location.lat,
+          place.location.lng,
+        ],
+        (_, result) => {
+          console.log(result);
+          resolve(result);
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+
+  return promise;
 }
