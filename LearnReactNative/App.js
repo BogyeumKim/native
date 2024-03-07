7f93653ed5fc09944c9db44babefec3b8b78b7ea
bogyeumkim <namastae112@gmail.com>
2024-03-07 23:09:41 +0900

    3.2.1 keyboardAvoidingView 적용

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {View, Text,StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import DateHead from './components/DateHead';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
@@ -12,18 +18,25 @@ const App = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
-        <DateHead date={today} />
-        <Empty />
-        <AddTodo />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          style={styles.avoid}>
+          <DateHead date={today} />
+          <Empty />
+          <AddTodo />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
-  block : {
-    flex :1
+  block: {
+    flex: 1,
+  },
+  avoid : {
+    flex : 1,
   }
-})
+});
 
 export default App;
