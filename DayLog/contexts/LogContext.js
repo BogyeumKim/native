e5ae18c5587daa9db5348f98c8babf456afcffd3
bogyeumkim <namastae112@gmail.com>
2024-03-10 21:39:50 +0900

    6.3.2.1 커스텀 Header

--- DayLog/contexts/LogContext.js
+++ DayLog/contexts/LogContext.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import {createContext, useState} from 'react';
+// import React from 'react';
+// import {createContext, useState} from 'react';
 
-const LogContext = createContext();
+// const LogContext = createContext();
 
-export function LogContextProvider({children}) {
-  const [text, setText] = useState('');
-  return (
-    <LogContext.Provider value={{text, setText}}>
-      {children}
-    </LogContext.Provider>
-  );
-}
+// export function LogContextProvider({children}) {
+//   const [text, setText] = useState('');
+//   return (
+//     <LogContext.Provider value={{text, setText}}>
+//       {children}
+//     </LogContext.Provider>
+//   );
+// }
 
-export default LogContext;
+// export default LogContext;
