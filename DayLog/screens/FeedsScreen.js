48940668356266e0a15fc1f825244f9e839e74fb
bogyeumkim <namastae112@gmail.com>
2024-03-10 22:23:39 +0900

    6.4 글 목록 보여주기

--- DayLog/screens/FeedsScreen.js
+++ DayLog/screens/FeedsScreen.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import {StyleSheet, View} from 'react-native';
 import FloatingWriteButton from '../components/FloatingWriteButton';
 import LogContext from '../contexts/LogContext';
+import FeedList from '../components/FeedList';
 
 function FeedsScreen() {
   const {logs} = useContext(LogContext);
 
   return (
     <View style={styles.block}>
+      <FeedList logs={logs} />
       <FloatingWriteButton />
     </View>
   );
