4529657a99f7407cd334568d4f4c5572c6c80d9c
bogyeumkim <namastae112@gmail.com>
2024-03-10 22:39:21 +0900

    6.5.2 Animarted로 버튼 View State 설정

--- DayLog/components/FeedList.js
+++ DayLog/components/FeedList.js
@@ -2,14 +2,29 @@ import React from 'react';
 import {FlatList, StyleSheet, View} from 'react-native';
 import FeedListItem from './FeedListItem';
 
-function FeedList({logs}) {
+function FeedList({logs , onScrolledToBottom}) {
+  const onScroll = e => {
+    const {contentSize, layoutMeasurement, contentOffset} = e.nativeEvent;
+    const distanceFromBottom =
+      contentSize.height - layoutMeasurement.height - contentOffset.y;
+
+    if (distanceFromBottom < 50) {
+        onScrolledToBottom(true);
+        console.log('바닥과 가까움!');
+    } else {
+        onScrolledToBottom(false);
+        console.log('바닥과 멀어짐!');
+    }
+  };
+
   return (
     <FlatList
       data={logs}
       style={styles.block}
       renderItem={({item}) => <FeedListItem log={item} />}
-      keyExtractor={(log) => log.id}
+      keyExtractor={log => log.id}
       ItemSeparatorComponent={() => <View style={styles.separator} />}
+      onScroll={onScroll}
     />
   );
 }
