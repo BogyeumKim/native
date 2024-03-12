d473cb430c8afb1eee92b851b9a76097ed472b62
bogyeumkim <namastae112@gmail.com>
2024-03-12 22:50:13 +0900

    12.3.1.3 children Props 사용

--- LearnTypeScript/App.tsx
+++ LearnTypeScript/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Profile from './Profile';
+import {Text} from 'react-native';
 
 function App(): React.JSX.Element {
-  return <Profile name="John Doe" />;
+  return (
+    <Profile name="John Doe">
+      <Text>Hello TypeScript?</Text>
+    </Profile>
+  );
 }
 
 export default App;
--- LearnTypeScript/Profile.tsx
+++ LearnTypeScript/Profile.tsx
@@ -1,20 +1,27 @@
+import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 
 type Props = {
   name: string;
   isActive?: boolean;
   image?: string;
+  children: React.ReactNode;
 };
 
-function Profile({name, isActive,image = 'https://picsum.photos/200'}: Props) {
+function Profile({
+  name,
+  isActive,
+  image = 'https://picsum.photos/200',
+  children,
+}: Props) {
   return (
     <View style={isActive && styles.activeStyle}>
-        <Image source={{uri:image}}/>
+      <Image source={{uri: image}} />
       <Text>Name</Text>
+      <View>{children}</View>
     </View>
   );
 }
-
 
 export default Profile;
 
