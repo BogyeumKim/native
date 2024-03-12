15d3fb7cfe1c9e7f83670468d2dafb3306d14b91
bogyeumkim <namastae112@gmail.com>
2024-03-12 22:48:37 +0900

    12.3.1.2 기본값 Props 사용

--- LearnTypeScript/Profile.tsx
+++ LearnTypeScript/Profile.tsx
@@ -1,22 +1,25 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 
 type Props = {
   name: string;
   isActive?: boolean;
+  image?: string;
 };
 
-function Profile({name,isActive}: Props) {
+function Profile({name, isActive,image = 'https://picsum.photos/200'}: Props) {
   return (
     <View style={isActive && styles.activeStyle}>
+        <Image source={{uri:image}}/>
       <Text>Name</Text>
     </View>
   );
 }
 
+
 export default Profile;
 
 const styles = StyleSheet.create({
-    activeStyle : {
-        backgroundColor : 'yellow'
-    }
-})
+  activeStyle: {
+    backgroundColor: 'yellow',
+  },
+});
