813d9b14253907bb53b46a20ee6dd93e2d2d1d54
bogyeumkim <namastae112@gmail.com>
2024-03-16 00:31:59 +0900

    15.1.2 내비게이션 타입 재 정의

--- LearnTypeScript/RootStack.tsx
+++ LearnTypeScript/RootStack.tsx
@@ -1,42 +1,15 @@
-import {useNavigation, RouteProp, useRoute} from '@react-navigation/native';
-import {
-  NativeStackNavigationProp,
-  createNativeStackNavigator,
-} from '@react-navigation/native-stack';
-import {Button, Text, View} from 'react-native';
-import MainTab, {MainTabNavigationScreenParams} from './screens/MainTab';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View } from "react-native";
+import { RootStackParamList } from "./screens/types";
+import MainTab from "./screens/MainTab";
 
-type RootStackParamList = {
-  MainTab: MainTabNavigationScreenParams;
-  Detail: {
-    id: number;
-  };
-};
-
-export type RootStackNavigationProp =
-  NativeStackNavigationProp<RootStackParamList>;
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function DetailScreen() {
-  const {params} = useRoute<RouteProp<RootStackParamList, 'Detail'>>();
-  return (
-    <View>
-      <Text>{params.id}</Text>
-    </View>
-  );
-}
-
-function RootStack() {
+function RootStack():React.JSX.Element {
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        component={MainTab}
-        name="MainTab"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen component={DetailScreen} name="Detail" />
+      <Stack.Screen name="MainTab" component={MainTab} options={{headerShown : false}} />
+      <View>{/*TODO*/}</View>
     </Stack.Navigator>
   );
 }
