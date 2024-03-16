54b6cd1e8d632d2653a16a9b3661ee8a43f6b8c2
bogyeumkim <namastae112@gmail.com>
2024-03-16 23:59:39 +0900

    15.4.2 useQuery 사용

--- LearnTypeScript/screens/ArticlesScreen.tsx
+++ LearnTypeScript/screens/ArticlesScreen.tsx
@@ -1,6 +1,14 @@
 import {Text, View} from 'react-native';
+import { useQuery } from 'react-query';
+import { getArticles } from '../api/articles';
 
 function ArticlesScreen() {
+
+  const {data,isLoading} = useQuery('articles',getArticles);
+
+  console.log({data,isLoading})
+  
+
   return (
     <View>
       <Text>Articles</Text>
