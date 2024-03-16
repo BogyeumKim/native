71187d1fae8d4f50b397cc668278daaced4b5f4f
bogyeumkim <namastae112@gmail.com>
2024-03-17 00:27:36 +0900

    axios test 10.0.2.2

--- LearnTypeScript/screens/ArticlesScreen.tsx
+++ LearnTypeScript/screens/ArticlesScreen.tsx
@@ -1,17 +1,30 @@
 import {Text, View} from 'react-native';
-import { useQuery } from 'react-query';
-import { getArticles } from '../api/articles';
+import {useQuery} from 'react-query';
+import {getArticles} from '../api/articles';
+import axios from 'axios';
 
 function ArticlesScreen() {
 
+  // const axiosTest = async () => {
+  //   try {
+  //     const res = await axios.get('http://10.0.2.2:1337/articles');
+  //     const data = res.data;
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.error('Error fetching articles:', error);
+  //   }
+  // };
+
+  // axiosTest();
+  
+  
   const {data,isLoading} = useQuery('articles',getArticles);
 
-  console.log({data,isLoading})
-  
+  console.log({data,isLoading});
 
   return (
     <View>
-      <Text>Articles</Text>
+      <Text>Articlessdddds</Text>
     </View>
   );
 }
