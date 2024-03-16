94e61f25c66e422fc49fa7640372a27415cf27eb
bogyeumkim <namastae112@gmail.com>
2024-03-16 23:36:11 +0900

    15.3.3 QueryClientProvider

--- LearnTypeScript/App.tsx
+++ LearnTypeScript/App.tsx
@@ -4,12 +4,17 @@ import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './RootStack';
 import qs from 'qs';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
