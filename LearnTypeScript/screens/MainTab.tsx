5bb975da43a1d6ca96835c77ee52abc9f516f252
bogyeumkim <namastae112@gmail.com>
2024-03-17 18:42:18 +0900

    15.6 회원 인증 기능 컴포넌트생성

--- LearnTypeScript/screens/MainTab.tsx
+++ LearnTypeScript/screens/MainTab.tsx
@@ -2,16 +2,34 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {View} from 'react-native';
 import {MainTabParamList} from './types';
 import ArticlesScreen from './ArticlesScreen';
+import UserMenuScreen from './UserMenuScreen';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator<MainTabParamList>();
 
 function MainTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{
+      tabBarShowLabel : false,
+    }}>
       <Tab.Screen
         name="Articles"
         component={ArticlesScreen}
-        options={{title: '게시글목록'}}
+        options={{title: '게시글목록',
+        tabBarIcon : ({color,size}) => (
+          <MaterialIcons name="article" color={color} size={size} />
+        ),
+      }}
+      />
+      <Tab.Screen
+        name="UserMenu"
+        component={UserMenuScreen}
+        options={{
+          title: '게시글 목록',
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="person" color={color} size={size} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
---
+++ LearnTypeScript/screens/RegisterScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View} from 'react-native';
+  function RegisterScreen() {
+  return <View />;
+}
+
+export default RegisterScreen;
