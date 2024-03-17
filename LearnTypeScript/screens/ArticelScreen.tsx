bf24849ee80530b250a75fa5f71648dde6158ade
bogyeumkim <namastae112@gmail.com>
2024-03-17 18:33:34 +0900

    15.5.4 댓글 컴포넌트 만들기

--- LearnTypeScript/screens/ArticelScreen.tsx
+++ LearnTypeScript/screens/ArticelScreen.tsx
@@ -1,10 +1,13 @@
 import {RouteProp, useRoute} from '@react-navigation/native';
 import React from 'react';
-import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
+import {View, StyleSheet, Text, ActivityIndicator, FlatList} from 'react-native';
 import {RootStackParamList} from './types';
 import {useQuery} from 'react-query';
 import {getArticle} from '../api/articles';
 import {getComments} from '../api/comments';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import ArticleView from '../components/ArticleView';
+import CommentItem from '../components/CommentItem';
 
 type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
 
@@ -15,18 +18,39 @@ function ArticleScreen() {
   const articleQuery = useQuery(['article', id], () => getArticle(id));
   const commentsQuery = useQuery(['comments', id], () => getComments(id));
 
+  const {bottom} = useSafeAreaInsets();
+
   if (!articleQuery.data || !commentsQuery.data) {
     return (
       <ActivityIndicator size="large" style={styles.spinner} color="black" />
     );
   }
 
+  const {title, body, published_at, user} = articleQuery.data;
+
   return (
-    <View style={styles.block}>
-      <Text>{articleQuery.data.title}</Text>
-      <Text>{articleQuery.data.body}</Text>
-      <Text>{commentsQuery.data.length}개의 댓글</Text>
-    </View>
+    <FlatList
+      style={styles.flatList}
+      contentContainerStyle={[styles.flatListContent, {paddingBottom: bottom}]}
+      data={commentsQuery.data}
+      renderItem={({item}) => (
+        <CommentItem
+          id={item.id}
+          message={item.message}
+          publishedAt={item.published_at}
+          username={item.user.username}
+        />
+      )}
+      keyExtractor={(item) => item.id.toString()}
+      ListHeaderComponent={
+        <ArticleView
+          title={title}
+          body={body}
+          publishedAt={published_at}
+          username={user.username}
+        />
+      }
+    />
   );
 }
 
@@ -34,6 +58,13 @@ const styles = StyleSheet.create({
   block: {},
   spinner: {
     flex: 1,
+  },
+  flatList: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  flatListContent: {
+    paddingHorizontal: 12,
   },
 });
 
