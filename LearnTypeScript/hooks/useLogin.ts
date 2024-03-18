d8b06784dd16ba73d61c25b7d889ef3b536c7f96
bogyeumkim <namastae112@gmail.com>
2024-03-18 23:03:30 +0900

    15.8.3 인증토큰 설정

--- LearnTypeScript/hooks/useLogin.ts
+++ LearnTypeScript/hooks/useLogin.ts
@@ -4,6 +4,7 @@ import {Alert} from 'react-native';
 import {useUserState} from '../contexts/UserContext';
 import {useNavigation} from '@react-navigation/native';
 import {RootStackNavigationProp} from '../screens/types';
+import { applyToken } from '../api/client';
 
 export default function useLogin() {
   const [, setUser] = useUserState();
@@ -13,9 +14,11 @@ export default function useLogin() {
     onSuccess: data => {
       setUser(data.user);
       navigation.pop();
+      applyToken(data.jwt);
     },
     onError: (error: AuthError) => {
       console.log(error);
+      console.log(error.response?.data);
       /* TODO: 구현 예정 */
     },
   });
