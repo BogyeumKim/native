4c3d9be8ce090ac2e2c3b1ffdb38720141716a0f
bogyeumkim <namastae112@gmail.com>
2024-03-18 23:12:17 +0900

    15.8.5 MKKV 를 이용한 인증상태 유지

---
+++ LearnTypeScript/storages/authStorage.ts
@@ -0,0 +1,29 @@
+import { MMKV } from "react-native-mmkv";
+import { AuthResult } from "../api/types";
+
+const storage = new MMKV();
+
+const key = 'auth';
+
+const authStorage = {
+  async get() {
+    const rawData = await storage.getString(key);
+    if (!rawData) {
+      return null;
+    }
+    try {
+      const data: AuthResult = JSON.parse(rawData);
+        return data;
+      } catch (e) {
+        return null;
+      }
+  },
+  set(authResult: AuthResult) {
+    return storage.set(key, JSON.stringify(authResult));
+  },
+  clear() {
+    return storage.delete(key);
+  },
+};
+
+export default authStorage;
