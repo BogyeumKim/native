a3a5f83b4021fccd60e7d466ae9c444dccfcd875
bogyeumkim <namastae112@gmail.com>
2024-03-20 22:26:31 +0900

    15.8.6 로그인/회원가입 오류 처리하기

--- LearnTypeScript/hooks/useRegister.ts
+++ LearnTypeScript/hooks/useRegister.ts
@@ -3,12 +3,14 @@ import {AuthError, register} from '../api/auth';
 import {useUserState} from '../contexts/UserContext';
 import {useNavigation} from '@react-navigation/native';
 import {RootStackNavigationProp} from '../screens/types';
-import { applyToken } from '../api/client';
+import {applyToken} from '../api/client';
 import authStorage from '../storages/authStorage';
+import useInform from './useInform';
 
 export default function useRegister() {
   const [, setUser] = useUserState();
   const navigation = useNavigation<RootStackNavigationProp>();
+  const inform = useInform();
 
   const mutation = useMutation(register, {
     onSuccess: data => {
@@ -18,8 +20,12 @@ export default function useRegister() {
       authStorage.set(data);
     },
     onError: (error: AuthError) => {
-      console.log(error);
-      /* TODO: 구현 예정 */
+      const message =
+        error.response?.data?.data?.[0]?.messages[0].message ?? '회원가입 실패';
+      inform({
+        title: '오류',
+        message,
+      });
     },
   });
   return mutation;
